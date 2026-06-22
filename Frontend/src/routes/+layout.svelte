959f410a71c6d0e707e7794e33ff8407ab746aab
AlphaTechini <rehobothokoibu@gmail.com>
2026-06-22 08:42:25 +0100

    Add Logo.png as static favicon in app.html, remove dynamic import

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/Logo.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
