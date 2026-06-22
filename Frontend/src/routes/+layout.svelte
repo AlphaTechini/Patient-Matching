85aed74538ec6743a7dcc00fac1f7f4b72c3516f
AlphaTechini <rehobothokoibu@gmail.com>
2026-06-22 08:40:44 +0100

    Fix import: Logo.png (capitalized)

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/TrialMatch Logo.png';
+	import favicon from '$lib/assets/Logo.png';
 
 	let { children } = $props();
 </script>
